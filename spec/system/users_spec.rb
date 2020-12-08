6bd9dbcb15e97ff4a5ac549d05fbfb564de8f46e
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-08 17:54:29 +0900

    users modelの単体テストを実装

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+  it 'ログアウト状態では、ヘッダーに新規登録/ログインボタンが表示される' do
+  end
+
+  it 'ログイン状態では、ヘッダーにニックネーム、ログアウトボタンが表示される' do
+  end
+
+  it 'ヘッダーの新規登録/ログインボタンをクリックすることで、各ページへ遷移できる' do
+  end
+
+  it 'ヘッダーのログアウトボタンをクリックすることでログアウトができる' do
+  end
+end
