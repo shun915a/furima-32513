3cf2fea705d39233f1782478bbee3f1810110822
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-08 19:24:26 +0900

    Revert "validation"
    This reverts commit f61c7ef1a41ef6dff38c45592585b2d6909fce6f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Password must includes letter and number.'
-
-  with_options presence: true do
-    validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :birthday
-  end
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :birthday, presence: true
 end
