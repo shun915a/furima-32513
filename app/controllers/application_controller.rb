9527f6c5bc3e04f7768ed2e5fd8ccb6cc95b7bb7
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-08 19:24:57 +0900

    Revert "新規登録が可能"
    This reverts commit 70e533f0b81e4b3c8bd3d07c70adf148e4462168.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-      :sign_up, keys: %i[
-        nickname
-        first_name
-        first_name_kana
-        last_name
-        last_name_kana
-        birthday
-      ]
-    )
-  end
 end
