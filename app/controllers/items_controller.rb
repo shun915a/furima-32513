3884d66b5a17c0b928c81831529e7ef731af4270
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-09 16:08:50 +0900

    itemsのcontrollerとviewを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
+  # ログインしていないユーザーをログインページへ
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
   def index
+    # itemの情報を全て取得
+    @items = Item.all
+  end
+
+  def new
+    # itemの新しいインスタンスを作成
+    @item = Item.new
   end
 end
