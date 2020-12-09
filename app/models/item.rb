90f962951425e52b3d5e97d17e05cb61ffbd0bd3
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-09 16:02:10 +0900

    item model validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :info
+    validates :price,
+              numericality: {
+                greater_than_or_equal_to: 300,
+                less_than_or_equal_to: 9_999_999,
+                message: 'Price Out of setting range'
+              }
+    validates :image
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :item_scheduled_delivery_id
+  end
 end
