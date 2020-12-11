9d0634f82219523f44cf8b0bd697bd2c05a9ea24
shun915a <74646604+shun915a@users.noreply.github.com>
2020-12-11 16:29:35 +0900

    購入機能の基本的な部分を作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,32 @@ class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    order = Order.create(order_params(@item))
+    address = Address.new(address_params(order))
+    if address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params(item)
+    params.permit(:user_id, :item_id).merge(user_id: current_user.id, item_id: item.id)
+  end
+
+  def address_params(order)
+    params.permit(
+      :zip_code,
+      :prefecture_id,
+      :city,
+      :street,
+      :building,
+      :phone_number
+    ).merge(order_id: order.id)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    validates :zip_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'input correctly' }
+    validates :prefecture_id, numericality: { other_than: 0 }
+    validates :city
+    validates :street
+    validates :phone_number, format: { with: /\d/, message: 'input only number' }
+  end
 end
